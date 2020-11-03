54c6638653f1711167ec9b5936b6be31c0b72406
Damian Basoń <damian.bason@gmail.com>
2020-11-03 17:25:16 +0100

    01.11.2020

--- src/main/java/pl/damianbason/geolocation/Covid19Confirmed.java
+++ src/main/java/pl/damianbason/geolocation/Covid19Confirmed.java
@@ -12,8 +12,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class Covid19Confirmed {
